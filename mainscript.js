329cd6662111fdcf76a5c997a43c31c646109aa7
LehaoHuang <a1896897@adelaide.edu.au>
2025-05-16 01:11:13 +0930

    dsada

--- mainscript.js
+++ mainscript.js
@@ -1,12 +1,12 @@
-<script>
-    const swiper = new Swiper('.mySwiper', {
-      loop: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false
-      },
-      speed: 600,
-      slidesPerView: 1,
-      spaceBetween: 20,
-    });
-</script>
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+    speed: 600,
+    slidesPerView: 1,
+    spaceBetween: 20,
+  });
+});
