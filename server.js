60a65f1fc95a6f29596ad8fe8606df6aa9c14988
Rey <1786860383@qq.com>
2025-05-18 05:16:21 +0000

    May 18, 2025, 05:16:21 UTC

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api', authRoutes);
 // Initialize user table before startup
 init()
   .then(() => {
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 8080;
     app.listen(PORT, () => {
       console.log(`🚀 Server listening on http://localhost:${PORT}`);
     });
